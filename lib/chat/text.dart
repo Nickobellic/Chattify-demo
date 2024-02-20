27db4bae8067d8fedcaec8d46650b4a7d31d0b45
ARJUN RAHUL VIJI <avrahulkanna17@gmail.com>
2024-02-20 19:15:07 +0530

    Feature to just add Chat message.

--- lib/chat/text.dart
+++ lib/chat/text.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+/*import 'package:flutter/material.dart';
 
 
 /*class MyApp extends StatelessWidget {
@@ -19,10 +19,13 @@ import 'package:flutter/material.dart';
   }
 }*/
 
-class MyCustomForm extends StatelessWidget {
+class MyCustomForm extends StatefulWidget {
   const MyCustomForm({super.key});
 
   @override
+  State<MyCustomForm> createState() => _MyCustomFormState();
+
+  /*@override
   Widget build(BuildContext context) {
     return Material(
       
@@ -35,6 +38,120 @@ class MyCustomForm extends StatelessWidget {
             ),
           ),
         ),
+    );
+  }*/
+}
+
+class _MyCustomFormState extends State<MyCustomForm> {
+  // Create a text controller and use it to retrieve the current value
+  // of the TextField.
+  final myController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    myController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Fill this out in the next step.
+    return TextField(
+  controller: myController,
+)
+  }
+}*/
+
+
+
+
+
+
+import 'package:flutter/material.dart';
+
+
+
+
+// Define a custom Form widget.
+class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({super.key});
+
+  @override
+  State<MyCustomForm> createState() => _MyCustomFormState();
+}
+
+// Define a corresponding State class.
+// This class holds the data related to the Form.
+class _MyCustomFormState extends State<MyCustomForm> {
+  // Create a text controller and use it to retrieve the current value
+  // of the TextField.
+  final myController = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
+
+  double _paddingTop = 700.0;
+
+   @override
+  void initState() {
+    super.initState();
+    _focusNode.addListener(() {
+      if (_focusNode.hasFocus) {
+        setState(() {
+          _paddingTop = 400.0; // Update the padding when the TextField receives focus
+        });
+      } else {
+        setState(() {
+          _paddingTop = 700.0; // Reset the padding when the TextField loses focus
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    myController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Retrieve Text Input'),
+      ),
+      body: Padding(
+        
+        padding: EdgeInsets.fromLTRB(16, _paddingTop, 16, 16),
+        child: TextField(
+          focusNode: _focusNode,
+          controller: myController,
+          decoration: InputDecoration(
+            hintText: "Enter your message.."
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        // When the user presses the button, show an alert dialog containing
+        // the text that the user has entered into the text field.
+        onPressed: () {
+          setState(() {
+            _paddingTop = 700.0;
+          });
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                // Retrieve the text the that user has entered by using the
+                // TextEditingController.
+                content: Text(myController.text),
+              );
+            },
+          );
+        },
+        tooltip: 'Show me the value!',
+        child: const Icon(Icons.text_fields),
+      ),
     );
   }
 }
